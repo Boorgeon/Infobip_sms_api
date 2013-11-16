069a20eb3d87e9b802d97f96a46b11611957971c
Serge NTONG <ntongserges@yahoo.fr>
2013-11-16 02:42:27 -0800

    Update example.php

--- example.php
+++ example.php
@@ -19,18 +19,55 @@ $infobip->setPassword('password');
 $infobip = new Infobip_sms_api(array(
     Infobip_sms_api::USERNAME => 'username',
     Infobip_sms_api::PASSWORD => 'password'
- ));
+        ));
 
 // Get balance -------------------------------------------------
 $balance = $infobip->getBalance();
 
 // Return
 /*
-stdClass Object
-(
-    [value] => 0.7900
-    [currency] => €
-)
+  stdClass Object
+  (
+  [value] => 0.7900
+  [currency] => €
+  )
  */
 echo $balance->value;
 echo $balance->currency;
+
+// Send 1 SMS to 1 --------------------------------------------------------
+
+$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_XML); // With xml method
+$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_JSON); // With json method
+$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_PLAIN); // With plain method
+
+$message = new Infobip_sms_message();
+
+$message->setSender('Boorgeon'); // Sender name
+$message->setText('Hello World'); // Message
+$message->setRecipients('phone1');
+$message->setRecipients('phone1', 'messageID'); // With custom message id
+
+$infobip->addMessages(array(
+    $message
+));
+
+$results = $infobip->sendSMS();
+
+// Return
+
+/*      
+Array
+(
+    [0] => stdClass Object
+        (
+            [status] => 0
+            [messageid] => messageID
+            [destination] => phone1
+        )
+
+)        
+    }
+
+}
+ */
