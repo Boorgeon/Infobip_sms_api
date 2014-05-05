ac30adda0625358d38b2167e78cc08b79f64e296
Serge NTONG <ntongserges@yahoo.fr>
2014-05-05 09:29:23 -0700

    Update send_1_sms_to_1.php

--- examples/send_1_sms_to_1.php
+++ examples/send_1_sms_to_1.php
@@ -8,19 +8,20 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 // Begin script
 
+
 require_once 'Infobip_sms_api.php';
 
 // Send 1 SMS to 1 --------------------------------------------------------
 
-$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_XML); // With xml method
-$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_JSON); // OR With json method
-$infobip->setMethod(Boorgeon_Service_Sms_Infobip::OUTPUT_PLAIN); // OR With plain method
+$infobip->setMethod(Infobip_sms_api::OUTPUT_XML); // With xml method
+$infobip->setMethod(Infobip_sms_api::OUTPUT_JSON); // OR With json method
+$infobip->setMethod(Infobip_sms_api::OUTPUT_PLAIN); // OR With plain method
 
 $message = new Infobip_sms_message();
 
 $message->setSender('Boorgeon'); // Sender name
-$message->setText('Hello World'); // Message
-$message->setRecipients('phone1');
+$message->setText('Bonjour'); // Message
+$message->setRecipients('23794595605');
 //$message->setRecipients('phone1', 'messageID'); // With custom message id
 
 $infobip->addMessages(array(
